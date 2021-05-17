b0b130b480cce8b5f552b59038cbaf708e901d8e
dev-davidgomes <davidsmithcrlh@outlook.com>
2021-05-17 13:30:42 -0300

    entregando desafio criando variaveis

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let n = 'David'
+
+let i = 27
+
+let a = 2021 
+
+console.log ("meu ano de nascimento é" , a - i ) 
+
+let souhumano = true 
+
+let k = 17
+
+let j = 9 
+
+
+console.log ("resultado da soma entre 17 e 9 é ! = " , k + j)
